deb25d847a06621c58933405153274dd7cb50a9f
Olzzhas <olzhasayato@gmail.com>
2023-05-18 20:02:38 +0600

    update

---
+++ Makefile
@@ -0,0 +1,10 @@
+#protoc --go_out=invoicer --go_opt=paths=source_relative --go-grpc_out=invoicer --go-grpc_opt=paths=source_relative invoicer.proto
+
+generate_grpc_code:
+	protoc \
+	--go_out=invoicer \
+	--go_opt=paths=source_relative \
+	--go-grpc_out=invoicer \
+	--go-grpc_opt=paths=source_relative \
+	invoicer.proto
+
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/olzzhas/grpc-chatapp/invoicer"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type myInvoicerServer struct {
+	invoicer.UnimplementedInvoicerServer
+}
+
+func (s myInvoicerServer) Create(ctx context.Context, req *invoicer.CreateRequest) (*invoicer.CreateResponse, error) {
+	return &invoicer.CreateResponse{
+		Pdf:  []byte(req.From),
+		Docx: []byte("test"),
+	}, nil
+}
 
 func main() {
-	fmt.Println("G0g0g0o")
+	lis, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		log.Fatalf("cannot create listener: %s", err)
+	}
+
+	serverRegistrar := grpc.NewServer()
+	service := &myInvoicerServer{}
+
+	invoicer.RegisterInvoicerServer(serverRegistrar, service)
+
+	err = serverRegistrar.Serve(lis)
+	if err != nil {
+		log.Fatalf("impossible to serve: %s", err)
+	}
 }
